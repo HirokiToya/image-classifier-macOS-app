0502ede1bd2376d3c51acee5e7fe7795a94ba9d6
Toya Hiroki <t.hiro.0425@icloud.com>
2019-10-18 16:11:32 +0900

    VIPPER構成に変更．類似度読み取り処理実装．

---
+++ image-classifier-app/Interactor/Interactor.swift
@@ -0,0 +1,9 @@
+//
+//  Interactor.swift
+//  image-classifier-app
+//
+//  Created by 外谷弘樹 on 2019/10/01.
+//  Copyright © 2019 example. All rights reserved.
+//
+
+import Foundation
---
+++ image-classifier-app/Service/Api/jsonPayload.swift
@@ -0,0 +1,9 @@
+//
+//  jsonPayload.swift
+//  image-classifier-app
+//
+//  Created by 外谷弘樹 on 2019/10/14.
+//  Copyright © 2019 example. All rights reserved.
+//
+
+import Foundation
