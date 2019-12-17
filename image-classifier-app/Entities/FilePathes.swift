05405df2e28d8d551da8f24cbdc1958f9a1ae34d
Toya Hiroki <t.hiro.0425@icloud.com>
2019-12-17 11:24:50 +0900

    類似度ファイル追加

--- image-classifier-app/Entities/FilePathes.swift
+++ image-classifier-app/Entities/FilePathes.swift
@@ -20,7 +20,10 @@ class FilePathes {
     }
     
     // 類似度リストファイルのPathを取得します．
-    class func getSimilalityFilePath(fileName: String = "similality_list.json") -> URL? {
+    // 1. lexvec_similarity.json
+    // 2. wordnet_path_similarity.json
+    // 3. wordnet_wup_similarity.json
+    class func getSimilalityFilePath(fileName: String = "wordnet_wup_similarity.json") -> URL? {
         if let filePath = URL(string: dataFilePath + fileName){
             return filePath
         }
